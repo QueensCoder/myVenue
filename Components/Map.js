1ad7e88f9b900121b9b5b4f0f8e9893343db6fdf
Ozal Khan <ozalkhan196@gmail.com>
2018-10-19 13:27:16 -0400

    changed repo to make app more modular

--- Components/Map.js
+++ Components/Map.js
@@ -1,21 +1,21 @@
-import React, { Component } from "react";
-import { View } from "react-native";
-import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 
-const GOOGLE_MAPS_APIKEY = "AIzaSyBwkfxv_va0TvU54Aho_zaNWxbi6_-XJMM";
+const GOOGLE_MAPS_APIKEY = 'AIzaSyBwkfxv_va0TvU54Aho_zaNWxbi6_-XJMM';
 
 const burgerLob = { latitude: 40.7402, longitude: 73.9934 };
 
 const placesToEat = [
   {
-    tite: "HomeTown HotPot",
+    tite: 'HomeTown HotPot',
     coordinate: { latitude: 40.7194, longitude: -73.9968 },
     description: 'Best Hot Pot in NYC',
     image: '../bbq.jpeg'
   },
   {
-    title: "Picnic Garden",
-    coordinate: { latitude: 40.650002, longitude:  -73.949997 },
+    title: 'Picnic Garden',
+    coordinate: { latitude: 40.650002, longitude: -73.949997 },
     description: 'Best Korean BBQ in Queens',
     image: '../bbq.jpeg'
   }
@@ -41,25 +41,23 @@ export default class MapScreen extends Component {
             longitudeDelta: 0.0421
           }}
         >
-          
-            {placesToEat.map((place, index) => (
-              <Marker
+          {placesToEat.map((place, index) => (
+            <Marker
               style={{ flex: 1 }}
-                key={index}
-                title={place.title}
-                image={require('../bbq.jpeg')}
-                description={place.description}
-                coordinate={place.coordinate}
-                
-                
-              />
-            ))}
-          
+              key={index}
+              title={place.title}
+              image={require('../bbq.jpeg')}
+              description={place.description}
+              coordinate={place.coordinate}
+            />
+          ))}
 
-          <Marker  coordinate={burgerLob} title={'testing'} description={'this is a test!'} />
-          
+          <Marker
+            coordinate={burgerLob}
+            title={'testing'}
+            description={'this is a test!'}
+          />
         </MapView>
-
       </View>
     );
   }
